ad37fe18e9055a43ca453cd3ed836248ab9936aa
ahmedanis45 <ahmedanis4546@gmail.com>
2024-04-06 18:35:13 +0500

    solve operator logic

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+var p = document.getElementById("inp");
 function clk(e){
-    document.getElementById("inp").value += e
+    // p = document.getElementById("inp").value += e;
+    // last = p.value.slice(p.value.length-1);
+    last = p.value[p.value.length-1];
+    var arr = ["+", "-", "*", "/", "%"];
+    if(arr.indexOf(last) != -1 && arr.indexOf(e) != -1)
+    {
+        p.value = p.value.slice(0, -1) + e ;
+    }
+    else 
+    {
+        p.value += e;
+    }
 }
+
 function output() {
-   var a = document.getElementById("inp");
-   a.value = eval(a.value)
+   p.value = eval(p.value)
 }
+
+
 function clrA() {
     if (document.getElementById("inp")) {
         var check = prompt("Do you wnat to clear!");
@@ -17,6 +31,5 @@ function clrA() {
     
 }
 function clr() {
-    var b = document.getElementById("inp");
-    b.value = b.value.slice(0,-1)
+    p.value = p.value.slice(0,-1)
 }
